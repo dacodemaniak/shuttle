e8d207f9f9d7dc03cd5fdea573cb840faa3a4224
Jean-Luc Aubert <jean-luc.a@ideafactory.fr>
2020-11-20 07:09:54 +0100

    Add mercure packages

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
 ];
