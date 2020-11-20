6f1b1af7af316cb697356da6d9d7b6ddc48b5fcd
Jean-Luc <jean-luc.a@ideafactory.fr>
2020-11-20 16:46:12 +0100

    Queuing

--- src/Controller/PublishController.php
+++ src/Controller/PublishController.php
@@ -10,24 +10,46 @@ use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Mercure\Publisher;
 use Symfony\Component\Mercure\PublisherInterface;
 use Symfony\Component\Mercure\Update;
+use App\Entity\Customer;
+use App\Entity\Shuttle;
+use App\Entity\Booking;
+use App\Helper\EntityManagerTrait;
+use App\Message\Booking\BookingEnvelop;
+use Symfony\Component\Messenger\Transport\AmqpExt\AmqpStamp;
 
 class PublishController extends AbstractController
 {
+    use EntityManagerTrait;
     /**
      * @Route("/message", name="publishMessage")
      */
-    public function index(MessageBusInterface $bus, PublisherInterface $publisher, Request $request)
+    public function index(MessageBusInterface $bus, Request $request)
     {
-        // Create an update (as an envelop)
-        $update = new Update(
-            'https://example.com/my-private-topic',
-            json_encode([
-                'message' => $request->request->get('message')
-            ])
-        );
+        // Send a new resa call
+        $newResa = json_decode($request->getContent());
         
-        // Send the Update in the bus
-        $bus->dispatch($update);
+        $theShuttle = $this->em
+            ->getRepository(Shuttle::class)
+            ->find($newResa->resa);
+        $before = $theShuttle->getPlaces();
+        
+        $theCustomer = $this->em
+            ->getRepository(Customer::class)
+            ->find($newResa->customer);
+        
+        $booking = new Booking();
+        $booking
+            ->setCustomer($theCustomer)
+            ->setShuttle($theShuttle)
+            ->setPlaces($newResa->places);
+        
+        $stamp = new AmqpStamp(
+              'booking'
+                );
+        $message = new BookingEnvelop($booking);
+            
+        $bus->dispatch($message, [$stamp]);
+        
         //$publisher($update);
         
         //return $this->redirectToRoute('home');
--- src/MessageHandler/Booking/BookingHandler.php
+++ src/MessageHandler/Booking/BookingHandler.php
@@ -5,10 +5,22 @@ use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 use App\Message\Booking\BookingEnvelop;
 use App\Helper\Logger\LoggerTrait;
 use App\Helper\EntityManagerTrait;
+use Symfony\Component\Mercure\Update;
+use Symfony\Component\Messenger\MessageBusInterface;
 
 class BookingHandler implements MessageHandlerInterface {
     
     use LoggerTrait, EntityManagerTrait;
+    
+    /**
+     * 
+     * @var MessageBusInterface
+     */
+    private $bus;
+    
+    public function __construct(MessageBusInterface $bus) {
+        $this->bus = $bus;
+    }
     
     public function __invoke(BookingEnvelop $envelop) {
         $this->logger->info(
@@ -19,6 +31,17 @@ class BookingHandler implements MessageHandlerInterface {
         
         $this->em->persist($envelop->getBooking());
         $this->em->flush();
+        
+        // Create an update (as an envelop)
+        $update = new Update(
+            'https://example.com/my-private-topic',
+            json_encode([
+                'message' => $envelop->getBooking()->getShuttle()->getPlaces()
+            ])
+         );
+        
+        // Send the Update in the bus
+        $this->bus->dispatch($update);
     }
 }
 
